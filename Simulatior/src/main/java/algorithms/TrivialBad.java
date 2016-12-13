9efa4fff7ad6bf9f73fcb83a75494c857b8095f3
LinFelix <felix-voelker@web.de>
2016-12-14 00:08:31 +0100

    write TrivailBad Algorithm

--- Simulatior/src/main/java/algorithms/TrivialBad.java
+++ Simulatior/src/main/java/algorithms/TrivialBad.java
@@ -3,6 +3,9 @@
  */
 package algorithms;
 
+import core.Player;
+import core.TableOfPoints;
+
 /**
  * @author Felix Völker
  *
@@ -14,6 +17,20 @@ public class TrivialBad implements Player {
 	 */
 	public TrivialBad() {
 		// TODO Auto-generated constructor stub
+	}
+
+	public void setTableOfPoints(TableOfPoints tableOfPoints) {
+		// this method does nothing since TrivalBad does not consider points
+	}
+
+	public Boolean firstDraw() {
+		// always returns false
+		return Boolean.FALSE;
+	}
+
+	public Boolean nextDraw(Boolean EnemiesLastAnswer) {
+		// always returns false
+		return Boolean.FALSE;
 	}
 
 }
